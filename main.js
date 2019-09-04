b3e830ab9ca4fc8251cc35991408dc43e7d94614
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-09-04 14:52:13 -0500

    Preguntar localización de la carpeta del juego

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+// Dependencias
+const { app, BrowserWindow, dialog } = require('electron')
+const fs = require('fs')
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Variables
+const databaseLocation = 'database.json'
+let gameLocation
+
+// Ventana
 let win
 
 function createWindow () {
-  // Create the browser window.
+  // Crear ventana
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -14,42 +20,74 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
+	// Cargar el archivo
   win.loadFile('src/home/home.html')
 
-  // Open the DevTools.
+  // Abrir herramientas de desarrollador
   win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  // Emitido cuando la ventana es cerrada
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
-  })
+	})
+	
+	if (fs.existsSync(databaseLocation)) {
+		console.log('Existe')
+	}
+	else {
+		// Repetir hasta seleccionar carpeta correcta
+		do {
+			gameLocation = selectFolder()
+		} while (!searchSongsFolder())
+	}
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+/**
+ * Selecionar carpeta
+ */
+const selectFolder = () => {
+	// Solicitar carpeta al usuario
+	const folder = dialog.showOpenDialogSync({
+		title: 'Selecciona la carpeta de osu!',
+		properties: [
+			'openDirectory'
+		]
+	})
+
+	// Carpeta seleccionada
+	if (folder !== undefined) {
+		return folder[0]
+	}
+
+	// No se seleccionó una carpeta
+	return undefined
+}
+
+/**
+ * Buscar carpeta llamada 'Songs'
+ */
+const searchSongsFolder = () => {
+	if (gameLocation === undefined) {
+		app.exit(-1)
+	}
+	else if (fs.existsSync(path.join(gameLocation, 'Songs'))) {
+		return true
+	}
+	else {
+		return false
+	}
+}
