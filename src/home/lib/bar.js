e5021b9c73a1f8582ed544708ec0a6ead2963417
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-10-05 17:00:01 -0500

    Agregar shorcuts
    • Shortcuts agregados:
    Ctrl+U - Actualizar base de datos
    Ctrl+1 - Ordenar por artista
    Ctrl+2 - Ordenar por título
    F1 - Abrir "acerca de osu! player"

--- src/home/lib/bar.js
+++ src/home/lib/bar.js
@@ -1,6 +1,7 @@
 // Dependencias
 const { remote, ipcRenderer } = require('electron')
 const { shell } = remote
+const Mousetrap = require('mousetrap')
 
 /*
  * Barra
@@ -21,11 +22,103 @@ class Bar {
 		this.refreshButton = document.getElementById('refreshButton')
 		this.donateButton = document.getElementById('donateButton')
 		this.aboutButton = document.getElementById('aboutButton')
-
+		// Lista de menús
 		this.menuButtons = document.getElementsByClassName('menu-item')
+		// Lista de submenús
 		this.submenus = document.getElementsByClassName('submenu')
 
-		// Listeners de menús
+		// Establecer shortcuts
+		this.setShortcuts()
+
+		/*
+		 * Establecer listeners
+		 */
+		this.setMenuListeners()
+		this.setSubmenuListeners()
+		this.setSubmenuActions()
+	}
+
+	/**
+	 * Actualizar base de datos
+	 */
+	actionUpdateDatabase() {
+		ipcRenderer.send('refresh-database')
+	}
+
+	/**
+	 * Salir de la aplicación
+	 */
+	actionCloseApp() {
+		const window = remote.getCurrentWindow()
+		window.close()
+	}
+
+	/**
+	 * Ordenar por título
+	 */
+	actionOrderbyTitle() {
+		this.player.sortPlaylist(this.player.playlist, 'title')
+		this.player.addSongsToContainer(this.player.playlist)
+	}
+
+	/**
+	 * Ordenar por artista
+	 */
+	actionOrderbyArtist() {
+		this.player.sortPlaylist(this.player.playlist, 'artist')
+		this.player.addSongsToContainer(this.player.playlist)
+	}
+
+	/**
+	 * Refrescar ventana
+	 */
+	actionRefreshWindow() {
+		const window = remote.getCurrentWindow()
+		window.reload()
+	}
+
+	/**
+	 * Donaciones
+	 */
+	actionDonate() {
+		shell.openExternal('https://ko-fi.com/alexazumi')
+	}
+
+	/**
+	 * Abrir acerca de la aplicación
+	 */
+	actionOpenAbout() {
+		ipcRenderer.send('open-about')
+	}
+
+	/**
+	 * Establecer shortcuts
+	 */
+	setShortcuts() {
+		Mousetrap.bind('ctrl+u', this.actionUpdateDatabase.bind(this))
+		Mousetrap.bind('ctrl+1', this.actionOrderbyArtist.bind(this))
+		Mousetrap.bind('ctrl+2', this.actionOrderbyTitle.bind(this))
+		Mousetrap.bind('f1', this.actionOpenAbout.bind(this))
+	}
+
+	/**
+	 * Establecer listeners de acciones de los submenús
+	 */
+	setSubmenuActions() {
+		this.updateDatabaseButton.addEventListener('click', this.actionUpdateDatabase.bind(this))
+		this.exitAppButton.addEventListener('click', this.actionCloseApp.bind(this))
+		this.sortArtistButton.addEventListener('click', this.actionOrderbyTitle.bind(this))
+		this.sortTitleButton.addEventListener('click', this.actionOrderbyArtist.bind(this))
+		this.refreshButton.addEventListener('click', this.actionRefreshWindow.bind(this))
+		this.donateButton.addEventListener('click', this.actionDonate.bind(this))
+		this.aboutButton.addEventListener('click', this.actionOpenAbout.bind(this))
+	}
+
+	/**
+	 * Establecer listeners del menú
+	 */
+	setMenuListeners() {
+		// Pasar por cada objeto
 		for (let menu of this.menuButtons) {
 			// onClick
 			menu.addEventListener('click', (event) => {
@@ -58,8 +151,12 @@ class Bar {
 				}
 			})
 		}
+	}
 
-		// Listeners de submenus
+	/**
+	 * Establecer listeners de submenús
+	 */
+	setSubmenuListeners() {
 		for (let submenu of this.submenus) {
 			// Click
 			submenu.addEventListener('click', (event) => {
@@ -75,42 +172,6 @@ class Bar {
 				}
 			})
 		}
-
-		/*
-		 * Asignar listeners
-		 */
-		// Actualizar base de datos
-		this.updateDatabaseButton.addEventListener('click', () => {
-			ipcRenderer.send('refresh-database')
-		})
-		// Salir de la aplicación
-		this.exitAppButton.addEventListener('click', () => {
-			const window = remote.getCurrentWindow()
-			window.close()
-		})
-		// Ordenar por artista
-		this.sortArtistButton.addEventListener('click', () => {
-			this.player.sortPlaylist(this.player.playlist, 'artist')
-			this.player.addSongsToContainer(this.player.playlist)
-		})
-		// Ordenar por título
-		this.sortTitleButton.addEventListener('click', (event) => {
-			this.player.sortPlaylist(this.player.playlist, 'title')
-			this.player.addSongsToContainer(this.player.playlist)
-		})
-		// Refrescar ventana
-		this.refreshButton.addEventListener('click', () => {
-			const window = remote.getCurrentWindow()
-			window.reload()
-		})
-		// Abrir donaciones
-		this.donateButton.addEventListener('click', () => {
-			shell.openExternal('https://ko-fi.com/alexazumi')
-		})
-		// Abrir acerca de la aplicación
-		this.aboutButton.addEventListener('click', () => {
-			ipcRenderer.send('open-about')
-		})
 	}
 }
 
