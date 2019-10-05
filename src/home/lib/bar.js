187ca1bf434cb67edf5486d8900e4594fb1487a6
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-10-05 15:40:58 -0500

    Mejorar comportamiento del menú
    • El menú tendrá un comportamiento similar a un menú nativo de Windows

--- src/home/lib/bar.js
+++ src/home/lib/bar.js
@@ -22,6 +22,60 @@ class Bar {
 		this.donateButton = document.getElementById('donateButton')
 		this.aboutButton = document.getElementById('aboutButton')
 
+		this.menuButtons = document.getElementsByClassName('menu-item')
+		this.submenus = document.getElementsByClassName('submenu')
+
+		// Listeners de menús
+		for (let menu of this.menuButtons) {
+			// onClick
+			menu.addEventListener('click', (event) => {
+				for (let submenu of this.submenus) {
+					// Ocultar otros submenus
+					if (submenu.style.display === 'block') {
+						submenu.style.display = 'none'
+					}
+				}
+				// Obtener submenu y mostrar
+				const submenu = event.srcElement.getAttribute('submenu')
+				const element = document.getElementById(`submenu-${submenu}`)
+				element.style.display = 'block'
+			})
+
+			// onMouseOver
+			menu.addEventListener('mouseover', (event) => {
+				if (event.srcElement.classList.contains('menu-item')) {
+					let hasSubmenuOpen = false
+					for (let submenu of this.submenus) {
+						if (submenu.style.display === 'block') {
+							submenu.style.display = 'none'
+							hasSubmenuOpen = true
+						}
+					}
+					// Verificar si existe algún submenú abierto
+					if (hasSubmenuOpen) {
+						event.srcElement.click()
+					}
+				}
+			})
+		}
+
+		// Listeners de submenus
+		for (let submenu of this.submenus) {
+			// Click
+			submenu.addEventListener('click', (event) => {
+				// Ocultar elemento
+				event.srcElement.parentElement.style.display = 'none'
+			})
+			// onMouseLeave
+			submenu.addEventListener('mouseleave', (event) => {
+				// Ocultar elemento
+				if (!event.toElement.classList.contains('menu-item')) {
+					// Ocultar elemento
+					event.srcElement.style = 'none'
+				}
+			})
+		}
+
 		/*
 		 * Asignar listeners
 		 */
