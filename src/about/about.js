9243411cd816a79d8ac6484e5caffbaa4ff3ed89
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-10-11 18:23:14 -0500

    Control de "Acerca de"
    • Todas las acciones de la ventana "Acerca de" serán controladas por AngularJS

--- src/about/about.js
+++ src/about/about.js
@@ -1,22 +1,2 @@
-// Dependencias
-const $ = require('jquery');
-const { remote, shell } = require('electron');
 // Angular
 require('./lib/aboutApp');
-// Información
-const appInfo = require('../../package.json');
-
-
-/*
- * Listeners
- */
-$('#appRepository').click(() => {
-	shell.openExternal(appInfo.homepage);
-});
-
-$('#closeBtn').click(() => {
-	// Obtener ventana
-	const window = remote.getCurrentWindow();
-	// Cerrar
-	window.close();
-});
--- src/about/lib/aboutApp.js
+++ src/about/lib/aboutApp.js
@@ -1,4 +1,5 @@
 // Dependencias
+const { remote, shell } = require('electron');
 const angular = require('angular');
 const LocalizationManager = require('../../localization');
 // Información
@@ -8,12 +9,24 @@ const localization = new LocalizationManager();
 
 angular.module('aboutApp', [])
 	.controller('aboutController', function ($scope) {
+		// Localización
 		$scope.about = {
 			version: localization.getString('about.version'),
 			author: localization.getString('about.author'),
 			repository: localization.getString('about.repository'),
 			close: localization.getString('about.close')
 		};
+		// Información
 		$scope.author = appInfo.author;
 		$scope.version = appInfo.version;
+		// Eventos
+		$scope.openRepository = function () {
+			shell.openExternal(appInfo.homepage);
+		};
+		$scope.closeWindow = function () {
+			// Obtener ventana
+			const window = remote.getCurrentWindow();
+			// Cerrar
+			window.close();
+		};
 	});
